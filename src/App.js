c705251fe8372eecc4fcdfe7806b566d65204380
Aman Dwivedi <19dwivediaman@gmail.com>
2021-05-02 01:11:38 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
 import Navbar from './Navbar';
-import Home from './Home'
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-			<Navbar />
+      <Navbar />
       <div className="content">
-				<Home />
-			</div>
+        <Home />
+      </div>
     </div>
   );
 }
 
 export default App;
- 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,13 +1,19 @@
+  
 const Navbar = () => {
-	return (  
-		<nav className="Navbar">
-			<h1>The Dojo blog</h1>
-			<div className="links">
-				<a href="/">Home </a>
-				<a href="/create"> New Blog</a>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create" style={{ 
+          color: 'white', 
+          backgroundColor: 'gold',
+          borderRadius: '8px', 
+					boxShadow: '1px 2px 2px #f1356d'
+        }}>New Blog</a>
+      </div>
+    </nav>
+  );
 }
  
 export default Navbar;
